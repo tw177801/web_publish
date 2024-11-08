2ea316804696d9e35f618a1f7ab11ae886301c1b
hyeon <tw177801@gmail.com>
2024-11-08 12:48:59 +0900

    commit

--- javascript/3. control/4_switch.js
+++ javascript/3. control/4_switch.js
@@ -11,39 +11,39 @@
 */ 
 
 // 0: 월요일, 1: 화요일, 2: 수요일
-let useage = `[0]월요일, [1]화요일, [2]수요일`;
-let day = 5;
-let resultDay = undefined;
-switch(day){
-    case 0:
-        resultDay = "월요일"; break;
-    case 1:
-        console.log("화요일"); break;
-    case 2:
-        console.log("수요일"); break;
-    default:
-        console.log(`사용법: ${useage}`);
-}
+// let useage = `[0]월요일, [1]화요일, [2]수요일`;
+// let day = 5;
+// let resultDay = undefined;
+// switch(day){
+//     case 0:
+//         resultDay = "월요일"; break;
+//     case 1:
+//         console.log("화요일"); break;
+//     case 2:
+//         console.log("수요일"); break;
+//     default:
+//         console.log(`사용법: ${useage}`);
+// }
 
-console.log(`선택한 요일은 [${resultDay}] 입니다.`);    
+// console.log(`선택한 요일은 [${resultDay}] 입니다.`);    
 
 
 // 임의의 숫자를 입력받아 
 // 짝수이면 빨간 사과, 홀수이면 초록 사과를 출력해주세요.
-let number = 101;
-let result = undefined;
-switch(number % 2) {
-    case 0 :
-        result = '🍎';
-        break;
-    case 1 :
-        result = '🍏';
-        break;
-    default : 
-        result = '해당 과일 없음';
-}
+// let number = 101;
+// let result = undefined;
+// switch(number % 2) {
+//     case 0 :
+//         result = '🍎';
+//         break;
+//     case 1 :
+//         result = '🍏';
+//         break;
+//     default : 
+//         result = '해당 과일 없음';
+// }
 
-console.log(result);
+// console.log(result);
 
 
 // let number = 100;
@@ -58,3 +58,11 @@ console.log(result);
 //     default : 
 //         실행문;
 // }
+
+
+let useage = `[0]월요일, [1]화요일, [2]수요일`;
+let day = 5;
+let resultDay = (useage);
+if(!(resultDay === undefined)){
+    console.log(`선택한 요일은 [${resultDay}] 입니다.`);
+} 
