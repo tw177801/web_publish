631f2e5bbe7fca6b2fe417c2f248c0ad2c4b7aff
hyeon <tw177801@gmail.com>
2024-11-11 12:42:36 +0900

    commit

---
+++ javascript/4. function/1_basic.js
@@ -0,0 +1,21 @@
+/*
+    함수 정의 ::
+    function 함수명(파라미터(=매개변수)) {
+        실행 로직;
+    }
+
+    함수 호출 :: 함수명(파라미터 형식 일치):
+
+    내장(Built-in) 함수 : 자주 사용하는 기능을 함수로 구현하여 엔진에서 제공함
+    - parseInt(문자열); 문자열을 숫자로 변환하는 함수
+    ... 
+*/
+
+console.clear();
+
+let a = '100';
+console.log(a, typeof a);
+console.log(parseInt(a), typeof parseInt(a));
+console.log(a+100);
+console.log(parseInt(a)+100);
+
