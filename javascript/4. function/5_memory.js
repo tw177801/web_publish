4b627541cf57c6520b23653f958d8e624d0de1f1
hyeon <tw177801@gmail.com>
2024-11-11 16:29:51 +0900

    commit

---
+++ javascript/4. function/5_memory.js
@@ -0,0 +1,19 @@
+// function은 데이터 타입?? object
+// 주소값 heap 저장
+// 호출 시 주소값 불러옴
+
+console.clear();
+
+
+function add(a, b) {
+    console.log(a + b);
+}
+
+
+let sum = add;
+console.log(add);
+console.log(sum);
+// console.log(add(1, 2));
+add(1,3);
+sum(1,2);
+console.log('---프로그램 종료---');
