97f1cdf0a35fb1b8f411636640e37f27bdeac034
hyeon <tw177801@gmail.com>
2024-11-13 12:45:12 +0900

    commit

--- javascript/5. object/4_method.js
+++ javascript/5. object/4_method.js
@@ -1,15 +1,21 @@
 // 객체에서 기능을 구현하는 메소드  (객체 리터럴 안 포함)
 console.clear();
+
+let name = "ddd";
+
+
 const apple = {
-    name : "orange",
+    name : "사과", // 프로퍼티
     emoji : "🍊",
     color : "Red",
-    display : function () {console.log("🍊");
-    }
+    display : function () {console.log("🍊");}, //메소드
+    getName: function () {console.log(this.name);}, // call stack 저장 
+    getColor: function () {console.log(this.color);},
+    getEmoji: function () {console.log(this.emoji);}
 }
 
 
 console.log(apple);
-console.log(apple.name);
-console.log(apple.color);
-apple.display();
+apple.getName();
+apple.getColor();
+apple.getEmoji();
---
+++ javascript/5. object/5_constructor.js
@@ -0,0 +1,40 @@
+// 객체 생성자 함수 정의와 사용법 익히기 -> 메모리에 올려두는 것
+console.clear();
+
+function Fruit (name, color, emoji) {
+    //field 
+    this.name = name;
+    this.color = color;
+    this.emoji = emoji;
+
+    //method
+    this.display = () => console.log(this.emoji);
+}
+
+
+const apple = new Fruit("apple", "red", "🍎");
+console.log(apple);
+
+
+// // apple : property - name, color : method - display()
+// const apple = {
+//     name: "apple", 
+//     color: "red",
+//     display: function () {console.log("🍎");}
+// }
+
+// // orange : property - name, color : method - display()
+// const orange = {
+//     name: "orange", 
+//     color: "coral",
+//     display: function () {console.log("🍊");}
+// }
+
+// // lemon : property - name, color : method - display()
+// const lemon = {
+//     name: "lemon", 
+//     color: "yellow",
+//     display: function () {console.log("🍋");}
+// }
+
+
