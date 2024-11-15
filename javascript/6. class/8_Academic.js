8aad90a4ae4c1843e1f2990966c988a09a675743
hyeon <tw177801@gmail.com>
2024-11-15 12:45:45 +0900

    commit

--- javascript/6. class/8_Academic.js
+++ javascript/6. class/8_Academic.js
@@ -29,6 +29,14 @@ class Student extends Member {
     }
 
     get sno() { return this.#sno; }
+    // 전체 값을 반환하는 함수 정의
+    // 객체명.values() 
+    // values = () => [this.name, this.age, this.address, this.#sno];
+    
+    // 객체명.values
+    get values () {
+        return [this.name, this.age, this.address, this.#sno];
+    }
 
 }
 
@@ -40,6 +48,11 @@ class Professor extends Member {
         this.#subject = subject;
     }
     get subject() { return this.#subject; }
+
+    // 객체명.values
+    get values () {
+        return [this.name, this.age, this.address, this.#subject];
+    }
 
 }
 
@@ -53,6 +66,11 @@ class Parent extends Member {
 
     get cname() { return this.#cname; }
 
+    // 객체명.values
+    get values () {
+        return [this.name, this.age, this.address, this.#cname];
+    }
+
 }
 
 
@@ -65,85 +83,130 @@ class Employee extends Member {
     }
 
     get department() { return this.#department; }
+    // 객체명.values
+    get values () {
+        return [this.name, this.age, this.address, this.#department];
+    }
+
 
 }
 
 // signup 버튼 클릭시 호출되는 함수
 const signupCheck = () => {
     let type = document.querySelector("input[type=radio]:checked");
-    let sno = document.querySelector("#sno");
-    let name = document.querySelector("#name");
-    let age = document.querySelector("#age");
-    let address = document.querySelector("#address");
-    let cname = '';
-    let department = '';
-
+    let name, age, address, sno, subject, cname, department;
     //  type에 따라서 각각의 클래스 생성
     let member = null;
     switch(type.value) {
-        case '1': 
+        case '1':
+            name = document.querySelector("#student #name");   
+            age = document.querySelector("#student #age");   
+            address = document.querySelector("#student #address");   
+            sno = document.querySelector("#student #sno");   
+
             member = new Student(sno.value, 
                                 name.value, 
                                 age.value, 
                                 address.value); 
-                                break;
+            break;
 
         case '2': 
-            member = new Professor(name, 
-                                    age, 
-                                    address, 
-                                    subject); 
-                                    break;
+            name = document.querySelector("#professor #name");
+            age = document.querySelector("#professor #age");
+            address = document.querySelector("#professor #address");
+            subject = document.querySelector("#professor #subject");
+
+            member = new Professor(name.value, 
+                                    age.value, 
+                                    address.value, 
+                                    subject.value); 
+            break;
 
         case '3': 
-            member = new Parent(name, 
-                                age, 
-                                address, 
-                                cname); 
-                                break;
+            sno = document.querySelector("#parent #sno");
+            age = document.querySelector("#parent #age");
+            address = document.querySelector("#parent #address");
+            cname = document.querySelector("#parent #cname");
+
+
+
+            member = new Parent(sno.value, 
+                                age.value, 
+                                address.value, 
+                                cname.value); 
+            break;
 
         case '4': 
-            member = new Employee(name, 
-                                age, 
-                                address, 
-                                department); 
-                                break;
-        default : 
+            name = document.querySelector("#employee #name");
+            age = document.querySelector("#employee #age");
+            address = document.querySelector("#employee #address");
+            department = document.querySelector("#employee #department");
+
+
+            member = new Employee(name.value, 
+                                age.value, 
+                                address.value, 
+                                department.value); 
+            break;
+
+        // default : 내용 없으면 생략
     }
     
-    console.log(member);
+    // console.log(member);
+    // 자바스크립트로 생성되는 HTML를 Dynamic HTML (DHTML) 동적 HTML -> 기억
+
+    // let list = Object.keys(member); // ['name', 'age', 'address', 'sno']
+    // --> class의 필드값이 private인 경우에만 데이터를 가져올 수 없음!!!
+
     
+    // console.log(member.values[0]);
+
+    // let list = Object.keys(member); // ['name', 'age', 'address', 'sno']
+
+
+    let list = '';
+    member.values.forEach((item) => {
+        list += `<li>${item}</li>`;
+
+    });
+    let output = `<ul>${list}</ul>`;
+
+    document.querySelector("#result").innerHTML = output;
+
 } // end of signupCheck
 
-// display
+
+
+// display : 라디오 버튼 선택시 화면을 전환시키는 함수
 const display = (type) => {
     // type=1 학생폼
+
+
+    document.querySelector("#result").innerHTML = '';
+
+    document.querySelector('#student').style.display = "none";
+    document.querySelector('#professor').style.display = "none";
+    document.querySelector('#parent').style.display = "none";
+    document.querySelector('#employee').style.display = "none";
+
+
     if(type === '1') {
 
         document.querySelector('#student').style.display = "block";
-        document.querySelector('#professor').style.display = "none";
-        document.querySelector('#parent').style.display = "block";
-        document.querySelector('#employee').style.display = "block";
+
     } else if(type === '2') {
-        document.querySelector('#student').style.display = "none";
         document.querySelector('#professor').style.display = "block";
-        document.querySelector('#parent').style.display = "none";
-        document.querySelector('#employee').style.display = "none";
-        
+
     } else if(type === '3') {
-        document.querySelector('#student').style.display = "none";
-        document.querySelector('#professor').style.display = "none";
+
         document.querySelector('#parent').style.display = "block";
-        document.querySelector('#employee').style.display = "none";
-        
+       
     } else if(type === '4') {
-        document.querySelector('#student').style.display = "none";
-        document.querySelector('#professor').style.display = "none";
-        document.querySelector('#parent').style.display = "none";
+
         document.querySelector('#employee').style.display = "block";
         
-    }
-}
+    }   // if 
+} // end of display
 
 // const hong = new Student('1234', '홍길동', 20, '서울시 강남구'); // 학생
 // const smith = new Professor('smith', 40, '서울시 서초구', 'JavaScript'); // 교수
---
+++ javascript/7. built-in/1_wrapper.js
@@ -0,0 +1,42 @@
+// 기본 데이터 타입 (primitive Data Type) - number, string, boolean...
+// 기본 데이터 타입을 클래스로 정의한 것을 wrapper 클래스라 함.
+
+console.clear();
+
+/**Number  */
+
+let a = "123";
+if ( typeof a === "string") a = parseInt(a);
+if(a === 123) console.log(a);
+
+if(Number("123") === 123) console.log(`a=> ${a}`);
+
+// let aa = new Number(100);
+// console.log(Number.isNan(aa));
+
+
+let maxValue = Number.MAX_VALUE;
+let minValue = Number.MIN_VALUE;
+
+console.log(maxValue);
+console.log(minValue);
+
+let x = 100;
+let y = new Number(100);
+// new의 값 y에 넣는다.
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(x === y); // false가 나오는 이유 정리!!
+console.log(x === y.valueOf());
+
+
+/** String class + TEMPLETE LITERAL (``) */
+
+let str = "hello~";
+
+let str2 = new String("hello~");
+console.log(typeof str);
+console.log(typeof str2);
+console.log(str === str2.valueOf());
+
