f0e9f6916973b2d04940b4a344a3b03db2993d6b
hyeon <tw177801@gmail.com>
2024-11-27 15:58:50 +0900

    commit

--- javascript/13. json/kobisCommons.js
+++ javascript/13. json/kobisCommons.js
@@ -1,29 +1,35 @@
 /**
- * KMDB 영화 포스터 검색
+ * KMDB 영화 상세 정보
  */
+export async function kmdbMovieDetail(movieNm, openDt) {
+    const key = '59H5F0U0OFQB3R2261VM';
+    let url = `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/`;
+    url += `search_json2.jsp?collection=kmdb_new2&detail=Y`;
+    url += `&title=${movieNm}&releaseDts=${openDt}`;
+    url += `&ServiceKey=${key}`;
 
-
-export async function searchMoviePoster(movieNm, openDt) {
-
-
-    const key = `1387ed83604df30a0c5d9dfdea0cba00`;
-    const url = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=${key}&movieCd=${movieCd}`;
-    
     const result = await fetch(url);
-    const jsonData = await movie.json();
-
-    return result;
-
-
-
+    const jsonData = await result.json();
     
-
-
+    return jsonData;
 }
 
 
+/**
+ * KMDB 영화 포스터 검색
+ */
+export async function searchMoviePoster(movieNm, openDt) {
+    const key = '59H5F0U0OFQB3R2261VM';
+    let url = `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/`;
+    url += `search_json2.jsp?collection=kmdb_new2&detail=Y`;
+    url += `&title=${movieNm}&releaseDts=${openDt}`;
+    url += `&ServiceKey=${key}`;
 
-
+    const result = await fetch(url);
+    const jsonData = await result.json();
+    
+    return jsonData.Data[0].Result[0].posters.split("|")[0];
+}
 
 
 /**
