24aff9641d4dd1f8f688006b0f0cd737e28c9a1e
hyeon <tw177801@gmail.com>
2024-12-02 12:39:09 +0900

    commit

---
+++ react/my-app/src/App2.jsx
@@ -0,0 +1,13 @@
+import './App2.css';
+
+export default function App2() {
+    return (
+        <div>
+        <h1 className="title">React Test!!!</h1>
+        {/* <h3>Welcome to React~</h3> */}
+        </div>
+    );
+}
+
+
+//return 시 하나의 그룹핑, 문단 하나 return 불가
