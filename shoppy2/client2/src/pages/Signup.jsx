1297c754153a0b6c821bd1a84a656b87d299834b
hyeon <tw177801@gmail.com>
2025-01-16 16:42:00 +0900

    commit

--- shoppy2/client2/src/pages/Signup.jsx
+++ shoppy2/client2/src/pages/Signup.jsx
@@ -1,19 +1,75 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import '../styles/signup.css';
+import {validateSignup} from '../utills/funcValidate.js';
 
 export default function Signup() {
+
+    const msgRefs = {
+        'msgIdRef':useRef(null),
+        'msgPwdRef':useRef(null),
+        'msgCpwdRef':useRef(null),
+        'msgNameRef':useRef(null),
+        'msgPhoneRef':useRef(null),
+        'msgEmailnameRef':useRef(null),
+        'msgEmaildomainRef':useRef(null)
+    }
+
+    const refs = {
+        'idRef':useRef(null),
+        'pwdRef':useRef(null),
+        'cpwdRef':useRef(null),
+        'nameRef':useRef(null),
+        'phoneRef':useRef(null),
+        'emailnameRef':useRef(null),
+        'emaildomainRef':useRef(null)
+    }
+
+    const initFormData = {
+        'id':'',
+        'pwd':'',
+        'cpwd':'',
+        'name':'',
+        'phone':'',
+        'emailname':'',
+        'emaildomain':''
+    }
+
+
+    
+
+    const [formData, setFormData] = useState(initFormData);
+
+    //submit
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(validateSignup(refs, msgRefs)) {
+
+            console.log('formData--->>', formData);
+        }
+        
+    };
+
+    // change
+    const handleChangeForm = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]:value});        
+    };
+
+
     return (
         <div className="content">
             <h1 className="center-title">SIGINUP</h1>
-            <form action="#" method="post" className="signup-form">
+            <form className="signup-form" onSubmit={handleSubmit}>
                 <ul>
                     <li>
                         <label for="" ><b>아이디</b></label>
-                        <span id="error-msg-id">아이디를 입력해주세요</span>
+                        <span ref={msgRefs.msgIdRef}>아이디를 입력해주세요</span>
                         <div>
                             <input type="text" 
                                     name="id"
                                     id="id"
+                                    ref={refs.idRef}
+                                    onChange={handleChangeForm}
                                     placeholder = "아이디 입력(6~20자)" />
                             <button type="button" >중복확인</button>
                             <input type="hidden" id="idCheckResult" value="default" />
@@ -21,55 +77,66 @@ export default function Signup() {
                     </li>
                     <li>
                         <label for=""><b>비밀번호</b></label>
-                        <span id="error-msg-pwd">12자 이내의 비밀번호를 입력해주세요</span>
+                        <span ref={msgRefs.msgPwdRef}>12자 이내의 비밀번호를 입력해주세요</span>
                         <div>
                             <input type="password" 
                                     name="pwd"
                                     id="pwd"
+                                    ref={refs.pwdRef}
+                                    onChange={handleChangeForm}
                                     placeholder="비밀번호 입력(문자,숫자,특수문자 포함 6~12자)" />
                         </div>
                     </li>
                     <li>
                         <label for=""><b>비밀번호 확인</b></label>
-                        <span id="error-msg-cpwd">비밀번호 확인을 입력해주세요</span>
+                        <span ref={msgRefs.msgCpwdRef}>비밀번호 확인을 입력해주세요</span>
                         <div>
                             <input type="password" 
                                     name="cpwd"
                                     id="cpwd"
+                                    ref={refs.cpwdRef}
+                                    onChange={handleChangeForm}
                                     placeholder="비밀번호 재입력" />
                         </div>
                     </li>
                     <li>
                         <label for=""><b>이름</b></label>
-                        <span id="error-msg-name">이름을 입력해주세요</span>
+                        <span ref={msgRefs.msgNameRef}>이름을 입력해주세요</span>
                         <div>
                             <input type="text" 
                                     name="name"
                                     id="name"
+                                    ref={refs.nameRef}
+                                    onChange={handleChangeForm}
                                     placeholder="이름을 입력해주세요" />
                         </div>
                     </li>
                     <li>
                         <label for=""><b>휴대폰번호</b></label>
-                        <span id="error-msg-phone">휴대폰번호를 입력해주세요('-' 포함)</span>
+                        <span ref={msgRefs.msgPhoneRef}>휴대폰번호를 입력해주세요('-' 포함)</span>
                         <div>
                             <input type="text" 
                                     name="phone"
                                     id="phone"
+                                    ref={refs.phoneRef}
+                                    onChange={handleChangeForm}
                                     placeholder="휴대폰 번호 입력('-' 포함)" />
                         </div>
                     </li>
                     <li>
                         <label for=""><b>이메일 주소</b></label>
-                        <span id="error-msg-emailname">이메일 주소를 입력해주세요</span>
+                        <span ref={msgRefs.msgEmailnameRef}>이메일 주소를 입력해주세요</span>
                         <div>
                             <input type="text" 
                                     name="emailname"
                                     id = "emailname"
+                                    ref={refs.emailnameRef}
+                                    onChange={handleChangeForm}
                                     placeholder="이메일 주소" />
                             <span>@</span>       
                             <select name="emaildomain" 
-                                    id="emaildomain"  >
+                                    id="emaildomain"
+                                    ref={refs.emaildomainRef}  >
                                 <option value="default">선택</option>
                                 <option value="naver.com">naver.com</option>
                                 <option value="gmail.com">gmail.com</option>
@@ -78,7 +145,7 @@ export default function Signup() {
                         </div>
                     </li>
                     <li>
-                        <button type="button">가입하기</button>
+                        <button type="submit">가입하기</button>
                         <button type="reset">가입취소</button>
                     </li>
                 </ul>
--- shoppy2/client2/src/utills/funcValidate.js
+++ shoppy2/client2/src/utills/funcValidate.js
@@ -21,3 +21,57 @@
     return result;
 
 }
+
+
+export const validateSignup = (refs, msgRefs) => {
+
+
+        const refEntries = Object.entries(refs);    // {idRef:xxx,...}
+        const msgRefEntries = Object.entries(msgRefs);
+
+
+        console.log(refEntries);    // []
+        console.log(msgRefEntries);
+        
+        // refEntries 배열 객체와 msgRefEntries 배열 객체의 인덱스를 동일하게 체크한다!
+
+        for(let i=0; i<refEntries.length; i++) {
+            const item = refEntries[i];
+            const msgItem = msgRefEntries[i];
+            const name = item[0];
+            const ref = item[1];    // 데이터 입력폼 객체 주소 
+            const msgName = msgItem[0];
+            const msgRef = msgItem[1]; // 데이터 입력폼의 메시지 객체 주소 
+
+            if(name !== 'emaildomainRef') { //  ''
+                if(ref.current.value === '') {
+                    // alert('이메일 주소를 선택해주세요')
+                    msgRef.current.style.setProperty('color', 'red');
+                    ref.current.focus();
+                    return false;
+                } else if(name === 'emaildomainRef') {   // default
+                    if(ref.current.value === 'default') {
+                        ref.current.focus();
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+        }
+    
+        // map, forEach 
+
+        // let result = true;
+        // if(refs.idRef.current.value === '') {
+        //     alert("아이디를 입력해주세요");
+        //     refs.idRef.current.focus();
+        //     result = false;
+        // } else if(refs.pwdRef.current.value === '') {
+        //     alert("패스워드 입력해주세요");
+        //     refs.pwdRef.current.focus();
+        //     result = false;
+        // }
+
+        // return result;
+    
