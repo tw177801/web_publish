b26cf921cc699d4803a317d8beb62f26857b78c5
hyeon <tw177801@gmail.com>
2025-02-04 18:20:14 +0900

    commit

---
+++ project/web/src/Home/series.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function series() {
+    return (
+        <div className='slider-continer'>
+            <div className='content-title'>시리즈</div>
+            <div>
+
+            </div>
+            <div></div> 
+
+        </div>
+    );
+}
+
