2122583f9ed066ed7738e1439ebe9aad3d804c97
hyeon <tw177801@gmail.com>
2025-02-10 06:54:32 +0900

    commit

---
+++ test/snapcase/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],  // src 폴더 내 모든 파일에서 Tailwind 클래스 검색
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
