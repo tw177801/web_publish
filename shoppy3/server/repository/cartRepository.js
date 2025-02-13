5f217b16d215cb029cf036c517bf9ccf1c6e109b
hyeon <tw177801@gmail.com>
2025-02-13 15:05:27 +0900

    commit

--- shoppy3/server/repository/cartRepository.js
+++ shoppy3/server/repository/cartRepository.js
@@ -1,4 +1,27 @@
 import {db} from './db.js';
 
+/**
+ * 장바구니 추가 
+ */
 
+export const addCart = async({id, cartList}) => {
+    
+    cartList.map(async(item)=> {
+        // try {
+            const values= [item.size, item.qty, id, item.pid];
+            // console.log('values--> ', values);
+            const sql = `
+                insert into shoppy_cart(size, qty, id, pid, cdate)
+                    values(?, ?, ?, ?, now())
+            `;
+            const [result] = await db.execute(sql, values);
+            console.log(result.affectedRows);
+            
+        // } catch (error) {
+
+        // }
+    });
+
+    return '';
+}
 
